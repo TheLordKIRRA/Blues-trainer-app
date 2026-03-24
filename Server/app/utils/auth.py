676db756de70eafb6b62c7dbb42add5a944048f1
TheLordKIRRA <aeddy146@gmail.com>
2026-03-23 22:41:09 -0400

    Implement JWT creation function in auth.py

---
+++ Server/app/utils/auth.py
@@ -0,0 +1,10 @@
+import jwt
+import datetime
+from flask import current_app
+
+def create_jwt(user_id):
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }
+    return jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm="HS256")
