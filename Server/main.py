24d2a50b384030315f37af2e51be2ca7b99166a4
Colin-Greenwood <158375912+Colin-Greenwood@users.noreply.github.com>
2026-03-25 20:25:39 -0400

    Initialize Flask application with CORS and routes

---
+++ Server/main.py
@@ -0,0 +1,25 @@
+from flask import flask
+from falsk_cors import CORS
+from app.db.database import init_db, db
+from app.api.auth_routes import auth_bp
+from app,api.practice_routes import practice_bp
+
+def create_app():
+  app = flask(__name_)
+  CORS(app)
+
+app.config["SQALCHEMY_DATABASE_URI"] = "postgresql://postgres:passwrd@db:5432/bluestrainer"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "supersecretkey"
+
+init_db(app)
+
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
+app.register_blueprint(practice_bp, url_prefix="/api/practice")
+
+return app
+
+app = create_app()
+
+if __name__ == "__main__":
+app.run(debug=True, host="0.0.0.0")
