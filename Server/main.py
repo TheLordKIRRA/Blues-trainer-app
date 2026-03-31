f95e5d15cfc76cff39d03f907c73e4a8ac619f20
Colin-Greenwood <158375912+Colin-Greenwood@users.noreply.github.com>
2026-03-30 21:39:19 -0400

    Fix import errors and update database URI
    Updated database connection string and corrected import errors.

--- Server/main.py
+++ Server/main.py
@@ -8,7 +8,7 @@ def create_app():
   app = flask(__name_)
   CORS(app)
 
-app.config["SQALCHEMY_DATABASE_URI"] = "postgresql://postgres:passwrd@db:5432/bluestrainer"
+app.config["SQALCHEMY_DATABASE_URI"] = "root:1234@db:3306/blues_trainer_app"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "supersecretkey"
 
